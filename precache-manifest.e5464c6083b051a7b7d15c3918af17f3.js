513a048c4df5b1ae1982e9a6d4547696c263216e
aleksandr <vaytehovich.aleksandr@mail.ru>
2019-09-16 14:43:32 +0300

    Updates

---
+++ precache-manifest.e5464c6083b051a7b7d15c3918af17f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676eb52fb77aec2050d3749c87726c8c",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "11ba53d15e44ec287bae",
+    "url": "/weather/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7b746f6522bbadf4488d",
+    "url": "/weather/static/css/main.0f96fc14.chunk.css"
+  },
+  {
+    "revision": "11ba53d15e44ec287bae",
+    "url": "/weather/static/js/2.5706233d.chunk.js"
+  },
+  {
+    "revision": "7b746f6522bbadf4488d",
+    "url": "/weather/static/js/main.6fbad36c.chunk.js"
+  },
+  {
+    "revision": "34a118672da3072aec95",
+    "url": "/weather/static/js/runtime~main.1423296d.js"
+  },
+  {
+    "revision": "fc938f1d24259ff71caed8072e4c1ec5",
+    "url": "/weather/static/media/sky.fc938f1d.jpg"
+  }
+]);
